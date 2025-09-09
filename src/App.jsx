ab56fde5d4aed46be939e040c8da01a721085a61
defendimattia <defendi.mattia92@gmail.com>
2025-09-09 14:18:02 +0200

    Add starting routes and bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 
 function App() {
-
-
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/frogs" element={<FrogsPage />} />
+          <Route path="/frogs/:id" element={<FrogDetailsPage />} />
+          <Route path="/habitats" element={<HabitatsPage />} />
+          <Route path="/habitats/:id" element={<HabitatDetailsPage />} />
+          <Route path="/conservationStatuses" element={<ConservationStatusesPage />} />
+          <Route path="/conservationStatuses/:id" element={<ConservationStatusDetailsPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
 
     </>
   )
