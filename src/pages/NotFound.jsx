5df279a77852265603bad356868ffbd610fd1ea2
defendimattia <defendi.mattia92@gmail.com>
2025-09-10 15:21:53 +0200

    Add NotFound page with relative css and main color class on index.css

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,23 @@
-export default function NotFound() {
+import { Link } from 'react-router-dom';
 
+export default function NotFound() {
     return (
-        <>
-            <h1>Not found</h1>
-        </>
-    )
+        <div className="container my-5">
+            <h1 className="display-4 green-color-txt text-center mb-5">404 - Pagina non trovata</h1>
+
+            <div className="row align-items-center justify-content-center">
+
+                <div className="col-12 col-md-6 d-flex flex-column align-items-start mb-4 text-center px-0">
+                    <p className="lead mb-4">...Ops pare che la rana che stavi cercando sia saltata troppo lontano</p>
+                    <div className="d-flex justify-content-center justify-content-md-center w-100">
+                        <Link className="btn btn-success green-color-bg" to="/">Torna alla Home</Link>
+                    </div>
+                </div>
+
+                <div className="col-12 col-md-6 d-flex justify-content-center justify-content-md-end px-0">
+                    <img src="src/assets/images/NotFoundImg.png" className="img-fluid" alt="not found img" />
+                </div>
+            </div>
+        </div>
+    );
 }
